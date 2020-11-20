672d37ce22ed16927915057677c49a2f16f500ec
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-11-20 15:57:06 +0100

    Day7 progress

---
+++ src/day7.jl
@@ -0,0 +1,23 @@
+
+
+function parse_input(filename)
+    open(filename) do file
+        Dict(begin
+            parts = split(l)
+            
+            if length(parts) == 3
+                
+            elseif length(parts) == 4
+                
+            elseif length(parts) == 5
+                
+            end
+        end
+        for l in eachline(file))
+    end
+end
+
+
+function part1()
+    
+end
