4c1087de348fd4be4f7911c40f1cd1ba9b812f12
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-11-20 18:44:28 +0100

    Day8

--- src/day8.jl
+++ src/day8.jl
@@ -1,6 +1,17 @@
 
 function part1()
     open("inputfiles/day8/input.txt") do io
-        
+        sum(length(l) - length(Meta.parse(l)) for l in eachline(io))
+    end
+end
+
+
+function part2()
+    open("inputfiles/day8/input.txt") do io
+        buff = IOBuffer()
+        sum(begin
+            show(buff, l)
+            length(String(take!(buff))) - length(l)
+        end for l in eachline(io))
     end
 end
